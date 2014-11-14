301c9254b8b31ebed613e8d8ea5d80bd51c16de5
Silviya Sotirova <silviya.soti@gmail.com>
2014-11-14 23:43:43 +0000

    New CodeForGood.py

--- CodeForGood/CodeForGood.py
+++ CodeForGood/CodeForGood.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+users = [
+    {
+        'name':'Sisi'
+    },
+    {
+        'name':'Lachezar'
+    }
+]
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/api/users')
+def get_users():
+    return jsonify({'users':users})
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
