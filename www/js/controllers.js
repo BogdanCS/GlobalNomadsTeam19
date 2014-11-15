f797aff9cb61a6e761ea3fc3bf31049bb1bbc7f7
kstanski <kajetanstanski@gmail.com>
2014-11-15 09:44:03 +0000

    fancy stuff

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,11 +3,15 @@ var appControllers = angular.module('CodeForGoodApp.controllers', []);
 /* I'm creating two arrays of JS objects only to mimic the future app behaviour  */
 
 appControllers.controller('mainController', function($scope, $http) {
-    $scope.users = [
-    {name: "user1", country: "Africa"},
-    {name: "user2", country: "Africa"},
-    {name: "user3", country: "Africa"},
-    ];
+
+	var url = 'http://ec2-54-170-122-68.eu-west-1.compute.amazonws.com:8000/allusers/';
+	$http.get(url).
+		success(function(data)) {
+			$scope.users = data;
+			console.log(data);
+		});
+
+
 
     $scope.chooseUser = function($index) {
     	var username = $scope.users[$index].name;
@@ -17,6 +21,9 @@ appControllers.controller('mainController', function($scope, $http) {
 });
 
 appControllers.controller('messageController', function($scope, $http) {
+
+
+
     $scope.messages = [
     {text: "Hi blabla", timestamp: "00:12", type: 0},
     {text: "Hello blabla", timestamp: "00:12", type: 1},
@@ -27,7 +34,16 @@ appControllers.controller('messageController', function($scope, $http) {
     {text: "Hello blabla", timestamp: "00:12", type: 1},
     {text: "Hi blabla", timestamp: "00:12", type: 0},
     {text: "Yoyo blabla yo", timestamp: "00:12", type: 1},
-    {text: "Yoyo yo", timestamp: "00:12", type: 1}
+    {text: "Yoyo yo", timestamp: "00:12", type: 1},
+    {text: "Hi blabla", timestamp: "00:12", type: 0},
+    {text: "Hello blabla", timestamp: "00:12", type: 1},
+    {text: "Hi blabla", timestamp: "00:12", type: 0},
+    {text: "Yoyo blabla yo", timestamp: "00:12", type: 1},
+    {text: "Yoyo yo", timestamp: "00:12", type: 1},
+    {text: "Hi blabla", timestamp: "00:12", type: 0},
+    {text: "Hello blabla", timestamp: "00:12", type: 1},
+    {text: "Hi blabla", timestamp: "00:12", type: 0},
+    {text: "Yoyo blabla yo", timestamp: "00:12", type: 1}
     ];
 
     $scope.btnSend = function() {
