fb2bd8eff85541b7cbdebfbb87ff60e95989e6fa
kstanski <kajetanstanski@gmail.com>
2014-11-15 06:18:35 +0000

    message color now specifies its sender

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,24 +8,29 @@ appControllers.controller('mainController', function($scope, $http) {
     {name: "user2", country: "Africa"},
     {name: "user3", country: "Africa"},
     ];
+
+    $scope.chooseUser = function($index) {
+    	var username = $scope.users[$index].name;
+    	alert(username);
+    }
+
 });
 
 appControllers.controller('messageController', function($scope, $http) {
     $scope.messages = [
-    {text: "Hi blabla", timestamp: "00:12"},
-    {text: "Hello blabla", timestamp: "00:12"}
+    {text: "Hi blabla", timestamp: "00:12", type: 0},
+    {text: "Hello blabla", timestamp: "00:12", type: 1},
+    {text: "Hi blabla", timestamp: "00:12", type: 0},
+    {text: "Yoyo blabla yo", timestamp: "00:12", type: 1},
+    {text: "Yoyo yo", timestamp: "00:12", type: 1}
     ];
 
     $scope.btnSend = function() {
     	var msg = $("#sendId").val();
     	if (msg != "") {
-    		$scope.messages.push({text: $("#sendId").val(), timestamp: "00:12"});
+    		$scope.messages.push({text: $("#sendId").val(), timestamp: "00:12", type: 0});
     	}
     	$("#sendId").val("");
     }
 
-});
-
-appControllers.controller('btnSendController', function($scope) {
-    alert('clicked!');
 });
